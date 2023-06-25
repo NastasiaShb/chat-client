df9f22600c2554225079faf521e1a359578fafd0
NastasiaShb <116949641+NastasiaShb@users.noreply.github.com>
2023-06-25 20:26:46 +0300

    chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,7 +7,7 @@ import icon from "../images/emoji.svg";
 import styles from "../styles/Chat.module.css";
 import Messages from "./Messages";
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://onlinechat-jsth.onrender.com");
 
 const Chat = () => {
     const { search } = useLocation();
